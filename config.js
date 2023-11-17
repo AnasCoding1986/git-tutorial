10f3310a8ec853b6bd40ada61611ba027ef9af4e
Anas <anassust.scw@gmail.com>
2023-11-17 07:00:50 +0600

    version 3

--- config.js
+++ config.js
@@ -2,3 +2,4 @@ console.log("config js");
 console.log("version 1");
 console.log("third change");
 console.log("version 2");
+console.log("version 3");
--- index.js
+++ index.js
@@ -2,3 +2,4 @@ console.log("index js");
 console.log("version 1");
 console.log("some change");
 console.log("version 2");
+console.log("version 3");
